6b606d36ab554a0cf6bb0177d1f63fca71159ee6
Nafissa Hassan <nafissa.hassan@quartzy.com>
2024-05-22 23:08:15 -0400

    fix

--- addon/helpers/ember-power-select-is-selected.ts
+++ addon/helpers/ember-power-select-is-selected.ts
@@ -5,17 +5,17 @@ import { isEqual } from '@ember/utils';
 // TODO: Make it private or scoped to the component
 export function emberPowerSelectIsSelected([option, selected]: [any, any]/* , hash*/): boolean {
   if (selected === undefined || selected === null) {
-    return false;
+    return "false";
   }
   if (isEmberArray(selected)) {
     for (let i = 0; i < selected.length; i++) {
       if (isEqual(selected[i], option)) {
-        return true;
+        return "true";
       }
     }
-    return false;
+    return "false";
   } else {
-    return isEqual(option, selected);
+    return `${isEqual(option, selected)};`
   }
 }
 
